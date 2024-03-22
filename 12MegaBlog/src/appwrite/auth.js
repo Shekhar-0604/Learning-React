a7d8eeb795a384e510b5409602b242fc08b18c86
Shekhar <Shekhar209203@gmail.com>
2024-03-22 22:06:06 +0530

    changes in auth.js

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -1,3 +1,57 @@
-import config from '../conf/conf.js'
+import conf from '../conf/conf.js'
 import { Client, Account, ID} from "appwrite"
 
+export class AuthService {
+    client  = new Client();
+    account;
+    constructor() {
+        this.client.setEndpoint(conf.apiwriteUrl)
+                   .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount(email, password, name){
+        try {
+            const userAccount = await this.account.create(ID.unique(), name, email, password);
+            if (userAccount) {
+                // call another method
+                return this.login({email,password})
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login(email, password) {
+        try {
+            return await this.account.createEmailSession(email, password);
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser")
+        }
+        return null;
+    }
+
+    async logout(){
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Error in Logout")
+        }
+    }
+}
+
+const authService = new AuthService();
+
+
+export default authService;
